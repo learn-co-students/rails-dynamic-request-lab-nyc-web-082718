7b294ddbf5bf5486741fff330de73d76c9712aa2
seanpara <spara77@gmail.com>
2018-09-21 16:43:38 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,12 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    # @student = Student.new(first_name: params[:first_name], ;last_name: params[last_name:])
+  end
 end
