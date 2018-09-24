c21149530a7c1f7fdae1bbd8e1e4714122bfaf01
danmj <daniel.jung@stern.nyu.edu>
2018-09-24 13:53:11 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,9 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+    @students = Student.find_by(id: params[:id])
+    render :show
+  end
 end
